0b20ab3881e3a6632eb11d03c6cc4cc68ffdff47
Kshitij-cse <1719kshitij@gmail.com>
2024-04-11 20:38:42 +0530

    5

--- images_tab.py
+++ images_tab.py
@@ -16,27 +16,47 @@ def Image_tab(df):
     if st.button("Show remarks",key=647):
         df1 = firebase_data_loader2() 
         st.dataframe(df1) 
+    
+    col1, col2, col3 = st.columns((3))
+   
+    startDate = pd.to_datetime(datetime.today() - timedelta(days=1))
+    endDate = pd.to_datetime(datetime.today())
+    
+    with col1:
+        st.markdown('<p style="font-size:22px; color:blue; font-weight:bold;">Select Date</p>', unsafe_allow_html=True)
+    with col2:
+        date1 = pd.to_datetime(st.date_input("Start Date", startDate,key=99999))
+    with col3:
+        date2 = pd.to_datetime(st.date_input("End Date", endDate,key=10000))
 
-    date1 = pd.to_datetime(datetime.today() - timedelta(days=1))
     date1 = date1.replace(hour=23, minute=59, second=15)
-    date2 = pd.to_datetime(datetime.now())
+    date2 = pd.to_datetime(date2)
+    date2 = date2 + timedelta(hours=23,minutes=59)
     df = df[(df['Date'] > date1.tz_localize(None)) & (df['Date'] <= date2.tz_localize(None))]
     
     search_property_id = st.text_input("Search by Property ID")
-    col1, col2 = st.columns(2)
+    col1, col2,col3 = st.columns(3)
     if search_property_id:
         with col1:
+            st.subheader("Property Images")
             display_images(df, search_property_id, 'property_image')
         with col2:
-            display_images(df, search_property_id, 'receiver_image')
+            st.subheader("Property Old Images")
+            display_images(df, search_property_id, 'image')
+        with col3:
+            st.subheader("Receiver Images")
+            display_images(df, search_property_id, 'receiver_image')    
     else:
         with col1:
             st.subheader("Property Images")
             display_images(df, column='property_image')
         with col2:
+            st.subheader("Property Old Images")
+            display_images(df, column='image')
+        with col3:
             st.subheader("Receiver Images")
             display_images(df, column='receiver_image')
-
+           
 @st.cache_data
 def display_images(df, search_property_id=None, column=None):
     placeholder_image_path = "images.png"  
