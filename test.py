1ab3a38fae50bc671e2d791124f68787193fbafd
Kshitij-cse <1719kshitij@gmail.com>
2024-07-23 11:43:16 +0530

    n

--- test.py
+++
@@ -1,6 +0,0 @@
-import json
-
-with open('firebase-credentials.json') as f:
-    service_account_info = json.load(f)
-encoded_credentials = json.dumps(service_account_info)
-print(encoded_credentials)
